c03cdf2f1f1aa2d9b6da8c3859ed1d91edc2d687
ozmen <fateyozmen@gmail.com>
2022-09-17 15:01:17 +0300

    main branch degisiklik

--- src/kisiselKUllanim/GithubKullanimi.java
+++ src/kisiselKUllanim/GithubKullanimi.java
@@ -6,5 +6,9 @@ public class GithubKullanimi {
         System.out.println("Hello World");
 
         System.out.println("That's it");
+        System.out.println("main branch degisiklik 1.satir");
+        System.out.println("main branch degisiklik 2.satir");
+        System.out.println("main branch degisiklik 3.satir");
+
     }
 }
